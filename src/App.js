a59958f305d900d6b4922b28403b771870df2a42
Ikenna <iykee0333@gmail.com>
2022-04-21 22:07:30 +0100

    Modified routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components'; 
 import './App.css';
